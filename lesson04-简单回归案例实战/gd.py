9b26773adb45d1af1c31c8fc3ad3223e01228a00
shengshimeiyan <49482862+shengshimeiyan@users.noreply.github.com>
2021-04-12 17:45:19 +0800

    Update gd.py

--- lesson04-简单回归案例实战/gd.py
+++ lesson04-简单回归案例实战/gd.py
@@ -19,8 +19,8 @@ def step_gradient(b_current, w_current, points, learningRate):
         b_gradient += -(2/N) * (y - ((w_current * x) + b_current))
         w_gradient += -(2/N) * x * (y - ((w_current * x) + b_current))
     new_b = b_current - (learningRate * b_gradient)
-    new_w = w_current - (learningRate * w_gradient)
-    return [new_b, new_w]
+    new_m = w_current - (learningRate * w_gradient)
+    return [new_b, new_m]
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
     b = starting_b
